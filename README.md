d164d5311b05d75036de84f81742bc622016c1eb
CB-Lim <129813815+CB-Lim@users.noreply.github.com>
2024-10-21 16:04:39 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# IHSetPrerpocess
+
+Preprocessing the dataset for IH-SET software
+
+## Installation and use
+
+To install this module use:
+
+```sh
+pip install https://github.com/IHCantabria/IHSetPreprocess/archive/refs/tags/latest.zip
+```
+
+Run tests to validate:
+
+## Documentation
+
+Documentation is available at https://ihcantabria.github.io/IHSetPreprocess
+
+## Credits
+
+Developed and maintained by Lim, Changbin @ IHCantabria.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IHSetLim',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'xarray',
+        'numba',
+        'datetime',
+        'spotpy',
+        'IHSetCalibration @ git+https://github.com/defreitasL/IHSetCalibration.git'
+    ],
+    author='Changbin Lim',
+    author_email='limcs@unican.es',
+    description='IH-SET Lim et al. (2022)',
+    url='https://github.com/IHCantabria/IHSetLim',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
