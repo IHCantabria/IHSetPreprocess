a87cf80e3477926e1c8e382e9d45c145f7a00f25
CB-Lim <129813815+CB-Lim@users.noreply.github.com>
2024-10-28 13:12:41 +0100

    Update test_preprocess.py

--- src/IHSetPreprocess/tests/test_preprocess.py
+++ src/IHSetPreprocess/tests/test_preprocess.py
@@ -3,22 +3,8 @@ from IHSetPreprocess import IHSetPreprocess
 import xarray as xr
 
 wrkDir = os.getcwd()
+path = wrkDir+'/data'
 
-obs = IHSetPreprocess.obs_data(wrkDir+'/data')
-
-obs = xr.open_dataset(wrkDir+'/data/ens.nc')
-print(obs['Obs'].values)
-
-sl = IHSetPreprocess.sl_data(wrkDir+'/data')
-
-slv = xr.open_dataset(wrkDir+'/data/sl.nc')
-# print(slv['surge'].values)
-# print(slv['tide'].values)
-# print(slv['sl'].values)
-
-wav = IHSetPreprocess.wave_data(wrkDir+'/data')
-
-wav = xr.open_dataset(wrkDir+'/data/wav.nc')
-print(wav['Hs'].values)
-print(wav['Tp'].values)
-print(wav['Dir'].values)
+obs = IHSetPreprocess.obs_data(path)
+sl = IHSetPreprocess.sl_data(path)
+wav = IHSetPreprocess.wave_data(path)
