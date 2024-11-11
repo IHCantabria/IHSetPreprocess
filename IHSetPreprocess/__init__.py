cb94847f1635b4a6642353fe07d38220b3fcba54
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-11-11 10:06:25 +0100

    Correcting readers

--- IHSetPreprocess/__init__.py
+++ IHSetPreprocess/__init__.py
@@ -1,6 +1,6 @@
 """ Template """
 
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 __author__ = "Lucas de Freitas Pereira"
 __author_email__ = "lucas.defreitas@unican.es"
 __description__ = "IH-SET Preprocessing module"
