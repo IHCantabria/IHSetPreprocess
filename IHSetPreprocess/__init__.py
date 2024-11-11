4bd777648f260dbbd54620352b5ad4f964613936
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-11-11 10:00:50 +0100

    Correcting readers

--- IHSetPreprocess/__init__.py
+++ IHSetPreprocess/__init__.py
@@ -1,6 +1,6 @@
 """ Template """
 
-__version__ = "0.2.15"
+__version__ = "0.2.16"
 __author__ = "Lucas de Freitas Pereira"
 __author_email__ = "lucas.defreitas@unican.es"
 __description__ = "IH-SET Preprocessing module"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetPreprocess',
-    version='0.2.15',
+    version='0.2.16',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -10,8 +10,7 @@ setup(
         'xarray',
         'numba',
         'datetime',
-        'pandas',
-        'json'
+        'pandas'
     ],
     author='Lucas de Freitas Pereira',
     author_email='lucas.defreitas@unican.es',
